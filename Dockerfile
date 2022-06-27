0e533211c67bceb2f78963d41d66a80ce0b688d8
yusufatalay <yusuftahaatalay14@gmail.com>
2022-06-28 00:26:24 +0300

    App and database dockerized

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,10 @@
-FROM golang:1.17-alpine AS builder
-RUN ap
-WORKDIR /go/src/app
+#build stage
+FROM golang:alpine AS builder
+RUN apk add --no-cache git
+RUN mkdir /app
+ADD . /app
+WORKDIR /app
+RUN go build -o yolcuapp .
+
+EXPOSE 3000
+CMD ["/app/yolcuapp"]
--- database/database.go
+++ database/database.go
@@ -2,6 +2,7 @@ package database
 
 import (
 	"log"
+	"os"
 
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
@@ -13,7 +14,7 @@ var (
 
 func init() {
 	var err error
-	dsn := "host=localhost user=postgres password=Killer14 dbname=yolcu360DB port=5432 sslmode=disable"
+	dsn := os.Getenv("DB_STRING") //"host=localhost user=postgres password=Killer14 dbname=yolcu360DB port=5432 sslmode=disable"
 	DBConn, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		log.Fatal("Cannot connect to the database: ", err)
